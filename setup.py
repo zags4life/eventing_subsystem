439e77b976d306da86f6ef3436fa3ec5b80df2c9
Travis Avery <travisavery82@gmail.com>
2019-11-17 10:55:44 -0600

    making pip installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='eventing_subsystem',
+    version='1.0.0',
+    author='Travis Avery',
+    python_requires='>=3.7',
+    packages=find_packages()
+)
